6540356b3cb461fe1c1e4e5077218774e7d760e5
rioatchu-max <rioatchu@gmail.com>
2026-01-29 12:10:43 +0530

    commited

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,12 +1,29 @@
-import Confirmation from "./hospital-management/conformation/Conformation";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./hospital-management/navigation/Navigation";
+
+import Home from "./hospital-management/home/Home";
+import Specialties from "./hospital-management/Specialties/Specialties";
+import Doctor from "./hospital-management/doctor/Doctor";
+import About from "./hospital-management/about/About";
+import Contact from "./hospital-management/contact/Contact";
+import Confirmation from "./hospital-management/conformation/Conformation";
 
 const App = () => {
   return (
-    <>
-    <Navigation/>
-    <Confirmation/>
-    </>
+    <BrowserRouter>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/specialties" element={<Specialties />} />
+        <Route path="/doctors" element={<Doctor />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
+
 export default App;
+
